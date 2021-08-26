43833673d58c7271842348c427a3869914e81ae9
eggyyuliandika <eggyyuliandika@gmail.com>
2021-08-26 20:27:12 +0800

    commit

--- pages/index.js
+++ pages/index.js
@@ -29,25 +29,25 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="container">
-        <header className="header">
+      <div className="container-about">
+        <header className="header-about">
           <div>
             <p className="judul-content">ABOUT COMPANY</p>
             <p className="desc-content">SOME HISTORY</p>
           </div>
         </header>
-        <aside className="aside">
+        <aside className="aside-about">
           <h1>— 1920</h1>
-          <div className="desc1">
+          <div className="desc1-about">
             Amet et delectus accommodare his consul copiosae legendos at vix ad
             putent delectus delicata usu. Vidit dissentiet eos cu eum an brute
             copiosae hendrerit. Eos erant dolorum an. Per facer affert ut. Mei
             iisque mentitum moderatius cu.
           </div>
         </aside>
-        <nav className="nav">
+        <nav className="nav-about">
           <h1>— 1986</h1>
-          <div className="desc1">
+          <div className="desc1-about">
             Amet et delectus accommodare his consul copiosae legendos at vix ad
             putent delectus delicata usu. Vidit dissentiet eos cu eum an brute
             copiosae hendrerit. Eos erant dolorum an. Per facer affert ut.
@@ -56,15 +56,15 @@ export default function Home() {
             <button className="btn-more2">More</button>
           </div>
         </nav>
-        <main className="main">
+        <main className="main-about">
           <Image src="/furniture3.jpg" width="430" height="400" />
         </main>
       </div>
       <div className="display-services">
-        <div className="header1">
+        <div className="header-services">
           <div>
-            <div className="judul-content1">SERVICES</div>
-            <div className="desc-content1">WE DO IT</div>
+            <div className="judul-content-services">SERVICES</div>
+            <div className="desc-content-services">WE DO IT</div>
           </div>
         </div>
         <div className="container-img">
@@ -75,50 +75,52 @@ export default function Home() {
           <Image src="/furniture4.jpeg" width="280" height="250" />
         </div>
       </div>
-      <div className="container2">
-        <header className="header2">
+      <div className="container-news">
+        <header className="header-news">
           <div>
-            <p className="judul-content2">NEWS</p>
-            <p className="desc-content2">LATEST EVENTS</p>
+            <p className="judul-content-news">NEWS</p>
+            <p className="desc-content-news">LATEST EVENTS</p>
           </div>
         </header>
-        <aside className="aside2">
-          <p className="desc-aside2">IN A NUMBER OF RECENT JUDICAL</p>
-          <p className="desc1-aside2">12.09.2018</p>
-          <p className="desc2-aside2">
+        <aside className="aside-news">
+          <p className="desc-aside-news">IN A NUMBER OF RECENT JUDICAL</p>
+          <p className="desc1-aside-news">12.09.2018</p>
+          <p className="desc2-aside-news">
             Is we miles ready he might going. Own books built put civil fully
             blind fanny. Projection appearance of admiration no. As he totally
             cousins warrant besides ashamed do. Therefore by applauded acuteness
             sup-ported affection it. Except had sex limits county enough the
             figure former add. Do sang my he next mr soon.
           </p>
-          <div className="dis-btn-more-aside2">
-            <button className="btn-more-aside2">More</button>
+          <div className="dis-btn-more-aside-news">
+            <button className="btn-more-aside-news">More</button>
           </div>
         </aside>
-        <nav className="nav2">
-          <p className="desc-aside2">SPECIAL EDITION</p>
-          <p className="desc1-aside2">12.09.2019</p>
-          <p className="desc2-aside2">
+        <nav className="nav-news">
+          <p className="desc-aside-news">SPECIAL EDITION</p>
+          <p className="desc1-aside-news">12.09.2019</p>
+          <p className="desc2-aside-news">
             Admiration we surrounded possession he. Remarkably did increasing
             pccasional too its difficulty far especially. Known tiled but sorry
             joy balls. Bed sudden manner indeed fat now feebly. Therefore by
             applaude acuteness supported affection it.
           </p>
-          <div className="dis-btn-more-aside2">
-            <button className="btn-more-aside2">More</button>
+          <div className="dis-btn-more-aside-news">
+            <button className="btn-more-aside-news">More</button>
           </div>
         </nav>
-        <main className="main2">
-          <Image src="/furniture9.jpeg" width="580" height="380" />
+        <main className="main-news">
+          <Image src="/furniture9.jpeg" width="560" height="380" />
         </main>
       </div>
 
-      <div className="display3">
-        <div className="header3">
+      <div className="display-sign">
+        <div className="header-sign">
           <div>
-            <div className="judul-content1">SIGN UP FOR COMPANY NEWS</div>
-            <div className="desc-content3">
+            <div className="judul-content-services">
+              SIGN UP FOR COMPANY NEWS
+            </div>
+            <div className="desc-content-sign">
               YOU WILL RECEIVE NOTIFICATIONS ABOUT NEW COMPANY NEWS
             </div>
             <div className="container-input">
@@ -127,7 +129,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="container5">
+        <div className="container-sign">
           <div className="box1">
             <Image src="/furniture7.jpg" width="120" height="100" />
           </div>
@@ -146,11 +148,11 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="container4">
-        <header className="header4">
-          <div className="desc-header4">
-            <div className="cont4-desc1">Mr. GAVONOV</div>
-            <div className="cont4-desc2">
+      <div className="container-footer">
+        <header className="header-footer">
+          <div className="desc-header-footer">
+            <div className="cont-footer-desc1">Mr. GAVONOV</div>
+            <div className="cont-footer-desc2">
               Far concluded not his something extremity. Want four we face an he
               gate. On he of played he ladies answer little though nature.
               Blessing oh do pleasure as so formerly. Took four spot soon led
@@ -158,8 +160,8 @@ export default function Home() {
             </div>
           </div>
           <div>
-            <div className="const4-desc3">USEFUL LINKS</div>
-            <div className="const4-desc4">
+            <div className="const-footer-desc3">USEFUL LINKS</div>
+            <div className="const-footer-desc4">
               <Link href="/">
                 <div>Vacancy</div>
               </Link>
@@ -172,8 +174,8 @@ export default function Home() {
             </div>
           </div>
           <div>
-            <div className="const4-desc3">CONTACT</div>
-            <div className="const4-desc5">
+            <div className="const-footer-desc3">CONTACT</div>
+            <div className="const-footer-desc5">
               <div>Address : pr-t Tekstilshikov, d. 17, office 3</div>
               <div>Number : 8 (800) 987-65-43</div>
               <div>E-mai/ : Interior_life@yandex.ru</div>
